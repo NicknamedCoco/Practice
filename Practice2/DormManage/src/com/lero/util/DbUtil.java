433088ebcf02c2ef4bfd2c40131618768e71556f
NicknamedCoco <one.dayinaug@gmail.com>
2021-01-14 17:27:36 +0800

    练习2

---
+++ Practice2/DormManage/src/com/lero/util/DbUtil.java
@@ -0,0 +1,31 @@
+package com.lero.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbUtil {
+	
+	public Connection getCon() throws Exception {
+		System.out.println(PropertiesUtil.getValue("jdbcName"));
+		Class.forName(PropertiesUtil.getValue("jdbcName"));
+		Connection con = DriverManager.getConnection(PropertiesUtil.getValue("dbUrl"), PropertiesUtil.getValue("dbUserName"), PropertiesUtil.getValue("dbPassword"));
+		return con;
+	}
+	
+	public void closeCon(Connection con) throws Exception {
+		if(con!=null) {
+			con.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DbUtil dbUtil = new DbUtil();
+		try {
+			dbUtil.getCon();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("no");
+			e.printStackTrace();
+		}
+	}
+}
